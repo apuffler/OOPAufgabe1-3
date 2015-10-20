8f84c763560ac5227116f9983130c0f18f0ec70f
Jovan Zivanovic <zivanovicjovanprivatemail@gmail.com>
2015-10-20 09:21:18 +0200

    Released-CO2 auskommentiert
    In diesem Stadium der Entwicklung ist diese Variable nicht notwendig.

--- CO2.java
+++ CO2.java
@@ -1,20 +1,20 @@
 public class CO2{
     private int bound;
-    private int released;
+    //private int released;
 
     public int getBoundCO2(){
         return this.bound;
     }
 
-    public int getReleasedCO2(){
+    /*public int getReleasedCO2(){
         return this.released;
-    }
+    }*/
 
     public void setBoundCO2(int boundCO2){
         this.bound = boundCO2;
     }
 
-    public void setReleasedCO2(int releasedCO2){
+    /*public void setReleasedCO2(int releasedCO2){
         this.released = releasedCO2;
-    }
+    }*/
 }
