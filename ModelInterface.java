aca7f8500a532777585135a47a9945d8d901aae0
apuffler <a.puffler@gmail.com>
2015-10-26 18:59:00 +0100

    Updated Classdiagramm, included a ModelInterface (which should be pretty much complete).

---
+++ ModelInterface.java
@@ -0,0 +1,27 @@
+interface ModelInterface<T>
+{	
+	//Conditions over years in form of (currently either a forest or an economy)
+	private ArrayList<T> conditions;
+	//int is the year in which this specialCondition will be applied, Object T will encorporate this specialCondition.
+	//For example: If there is a ForestFire, the number of trees will be reduced to 0.
+	private HashMap<int, SpecialCase> specialConditions;
+
+	//0 <= currentYear < conditions.size
+	private int currentYear;
+	private boolean cyclic;
+
+	//Applies the Model to (currently either a forest or an economy) for a number of years (mod length of conditions) if cyclic.
+	//This method will overwrite certain parameters (taken from the current condition) and leave others (if they are marked with a negative value) alone.
+
+	public ArrayList<T> applyTo(T t, int years);
+	//Applies the model for 1 year.
+	public T applyTo(T t);
+
+
+
+
+
+	public ModelInterface<T>(ArrayList<T> conditions, cyclic);
+
+
+}
