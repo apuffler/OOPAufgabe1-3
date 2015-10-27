3250a83100f43b1f1ecafa373d19623241c21d1d
JRyuzaki <zivanovicjovan@gmail.com>
2015-10-28 00:41:20 +0100

    Bug fixing

--- EnergyForest.java
+++ EnergyForest.java
@@ -5,13 +5,33 @@ public class EnergyForest extends Forest {
         super();
     }
 
-    public EnergyForest(int livingWoodAmount, int harvestedWoodAmount, int harvestedWoodHarvestPerYear, float harvestedWoodUsagePerYear, int deadWoodAmount, int deadWoodAdditionPerYear, float deadWoodDecayPerYear, Environment env, Economy eco){
-        super(livingWoodAmount, harvestedWoodAmount, harvestedWoodHarvestPerYear, harvestedWoodUsagePerYear, deadWoodAmount, deadWoodAdditionPerYear, deadWoodDecayPerYear, env, eco);
+    public EnergyForest(int livingWoodAmount, int harvestedWoodAmount, float harvestedWoodUsagePerYear, int deadWoodAmount, Environment env, Economy eco){
+        super(livingWoodAmount, harvestedWoodAmount, harvestedWoodUsagePerYear, deadWoodAmount, env, eco);
+        this.relaxationValue = 1;
+    }
+
+    public EnergyForest makeCopy(){
+        return new EnergyForest(this.livingWoodAmount, this.harvestedWoodAmount, this.harvestedWoodUsagePerYear, this.deadWoodAmount, this.env, this.eco);
     }
 
     public void updateForest(Environment e){
+        if(this.livingWoodAmount == 1) // log(1) = 0 so we have to tweak it a little
+            this.livingWoodAmount++;
+
+        float decay = ((float)Math.log((double)this.livingWoodAmount/ 2.0) / 100);
+        this.deadWoodAmount += (int)(this.livingWoodAmount * decay);
+        this.livingWoodAmount -= (int)(this.livingWoodAmount * decay);
+
         float multi = e.getHumidity() * e.getSunshine() * e.getPrecipitation() * e.getTemperature();
-        this.livingWoodAmount += (((Math.exp((double)multi) /100) +1) * this.livingWoodAmount);
+        this.livingWoodAmount += ((Math.exp((double)multi) /100) * this.livingWoodAmount);
+
+        // high harvest
+        float harvest = (float)((Math.log((double)this.livingWoodAmount) / 100) * 2.3);
+        this.harvestedWoodAmount += (int)(this.livingWoodAmount * harvest);
+        this.livingWoodAmount -= (int)(this.livingWoodAmount * harvest);
+
+        this.totallyUsedWood += (int)(this.harvestedWoodAmount * this.harvestedWoodUsagePerYear);
+        this.harvestedWoodAmount -= (int)(this.harvestedWoodAmount * this.harvestedWoodUsagePerYear);
         this.boundCO2 = this.livingWoodAmount + this.harvestedWoodAmount + this.deadWoodAmount;
     }
 }
