4b60d7e1d8d9a39548cd6434e1e762aac8828132
apuffler <a.puffler@gmail.com>
2015-10-27 17:02:53 +0100

    Test commit Models

--- ModelInterface.java
+++ ModelInterface.java
@@ -1,27 +1,26 @@
-interface ModelInterface<T>
-{	
+import java.util.ArrayList;
+public interface ModelInterface {	
 	//Conditions over years in form of (currently either a forest or an economy)
-	private ArrayList<T> conditions;
+	//private ArrayList<T> conditions;
 	//int is the year in which this specialCondition will be applied, Object T will encorporate this specialCondition.
 	//For example: If there is a ForestFire, the number of trees will be reduced to 0.
-	private HashMap<int, SpecialCase> specialConditions;
+	//private HashMap<int, SpecialCase> specialConditions;
 
 	//0 <= currentYear < conditions.size
-	private int currentYear;
-	private boolean cyclic;
+	//public int currentYear;
+
+	//public boolean cyclic;
 
 	//Applies the Model to (currently either a forest or an economy) for a number of years (mod length of conditions) if cyclic.
 	//This method will overwrite certain parameters (taken from the current condition) and leave others (if they are marked with a negative value) alone.
 
-	public ArrayList<T> applyTo(T t, int years);
+	//public ArrayList<T> applyTo(T t, int years);
 	//Applies the model for 1 year.
-	public T applyTo(T t);
+	//public Forest applyTo(Forest f);
 
 
 
-
-
-	public ModelInterface<T>(ArrayList<T> conditions, cyclic);
+	//public ModelInterface<T> (ArrayList<T> conditions,boolean cyclic);
 
 
 }
