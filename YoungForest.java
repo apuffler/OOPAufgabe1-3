4a44435ba449cf6a98d9c2e3554f9b5def190208
JRyuzaki <zivanovicjovan@gmail.com>
2015-10-28 00:42:07 +0100

    Bug fixing

--- YoungForest.java
+++ YoungForest.java
@@ -5,15 +5,27 @@ public class YoungForest extends Forest {
         super();
     }
 
-    public YoungForest(int livingWoodAmount, int harvestedWoodAmount, int harvestedWoodHarvestPerYear, float harvestedWoodUsagePerYear, int deadWoodAmount, int deadWoodAdditionPerYear, float deadWoodDecayPerYear, Environment env, Economy eco){
-        super(livingWoodAmount, harvestedWoodAmount, harvestedWoodHarvestPerYear, harvestedWoodUsagePerYear, deadWoodAmount, deadWoodAdditionPerYear, deadWoodDecayPerYear, env, eco);
+    public YoungForest(int livingWoodAmount, int harvestedWoodAmount, float harvestedWoodUsagePerYear, int deadWoodAmount, Environment env, Economy eco){
+        super(livingWoodAmount, harvestedWoodAmount, harvestedWoodUsagePerYear, deadWoodAmount, env, eco);
+        this.relaxationValue = 0;
+        }
+
+    public YoungForest makeCopy(){
+        return new YoungForest(this.livingWoodAmount, this.harvestedWoodAmount, this.harvestedWoodUsagePerYear, this.deadWoodAmount, this.env, this.eco);
     }
 
     public void updateForest(Environment e){
         float multi = e.getHumidity() * e.getSunshine() * e.getPrecipitation() * e.getTemperature();
         if(this.livingWoodAmount == 1) // log(1) = 0 so we have to tweak it a little
             this.livingWoodAmount++;
-        this.livingWoodAmount += ((((Math.log((double)this.livingWoodAmount * multi)) /100) + 1) * this.livingWoodAmount);
+
+        float decay = ((float)Math.log((double)this.livingWoodAmount/ 6.0) / 100);
+        this.deadWoodAmount += (int)(this.livingWoodAmount * decay);
+        this.livingWoodAmount -= (int)(this.livingWoodAmount * decay);
+
+        this.livingWoodAmount += (((Math.log((double)this.livingWoodAmount * multi)) /100) * this.livingWoodAmount);
+
+        // no harvest in a young forest, so there is also no totallyUsedWood
         this.boundCO2 = this.livingWoodAmount + this.harvestedWoodAmount + this.deadWoodAmount;
     }
 }
