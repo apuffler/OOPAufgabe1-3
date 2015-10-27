817945d6742fd5b72be9dc69bc5d5c8177170235
JRyuzaki <zivanovicjovan@gmail.com>
2015-10-27 23:53:15 +0100

    Needed to change the used LinkedList, so that scaling is applied.

--- LineGraph.java
+++ LineGraph.java
@@ -6,12 +6,12 @@ public class LineGraph extends SimulationGraph{
 	public void drawGraph(Graphics g){
 		Graphics2D g2D = (Graphics2D)g;
 
-		Pair<Double, Double> coordinates1 = this.graphValues.get(0);
+		Pair<Double, Double> coordinates1 = this.scaledValues.get(0);
 
-		for(int i = 0; i < this.graphValues.size(); ++i){
+		for(int i = 0; i < this.scaledValues.size(); ++i){
 			Pair<Double, Double> coordinates2;
-			if(i + 1 < this.graphValues.size())
-				coordinates2 = this.graphValues.get(i);
+			if(i + 1 < this.scaledValues.size())
+				coordinates2 = this.scaledValues.get(i);
 			else 
 				coordinates2 = coordinates1;
 
