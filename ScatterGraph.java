54b94ef0445b41c2b606118859530bff0f5e7083
JRyuzaki <zivanovicjovan@gmail.com>
2015-10-27 23:54:00 +0100

    Needed to change the used LinkedList, so that scaling is applied.

--- ScatterGraph.java
+++ ScatterGraph.java
@@ -16,11 +16,11 @@ public class ScatterGraph extends SimulationGraph{
 	public void drawGraph(Graphics g){
 		Graphics2D g2D = (Graphics2D)g;
 
-		for(Pair<Double,Double> coordinates : this.getGraphValues()){
+		for(Pair<Double,Double> coordinates : this.scaledValues){
 			double x = coordinates.getFirst() - this.radius;
 			double y = coordinates.getSecond() - this.radius;
 
-			g2D.draw(new Ellipse2D.Double(x, y, this.radius * 2, this.radius * 2));	
+			g2D.draw(new Ellipse2D.Double(x, y, this.radius * 2, this.radius * 2));
 		}
 	}
 }
