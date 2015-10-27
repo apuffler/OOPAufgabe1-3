fd3350b6be16aefe3d024227dad93c4e3a2b4a22
JRyuzaki <zivanovicjovan@gmail.com>
2015-10-27 16:57:13 +0100

    'runSimulation'-Methode geschrieben.

--- Simulation.java
+++ Simulation.java
@@ -1,15 +1,18 @@
 public class Simulation{
 	private EconomyModel economyModel;
-	private ForestModel forestModel;	
+	private ClimateModel climateModel;
+	private Forest forest;	
 
-	public Simulation(EconomyModel economyModel, ForestModel forestModel){
+	public Simulation(EconomyModel economyModel, ClimateModel climateModel, Forest forest){
 		this.economyModel = economyModel;
-		this.forestModel = forestModel;
+		this.climateModel = climateModel;
+		this.forest = forest;
 	}
 
 	public Simulation(){
 		this.economyModel = null;
-		this.forestModel = null;
+		this.climateModel = null;
+		this.forest = null;
 	}
 
 	public void setEconomyModel(EconomyModel economyModel){
@@ -20,15 +23,18 @@ public class Simulation{
 		return this.economyModel;
 	}
 
-	public void setForestModel(ForestModel forestModel){
-		this.forestModel = forestModel;
+	public void setClimateModel(ClimateModel climateModel){
+		this.ClimateModel = climateModel;
 	}
 
-	public ForestModel getForestModel(){
-		return this.forestModel;
+	public ClimateModel getClimateModel(){
+		return this.climateModel;
 	}
 
 	public void runSimulation() {
-		
+		this.forest = this.economyModel.applyTo(this.forest);
+		this.forest = this.climateModel.applyTo(this.forest);
+
+		forest.updateForest();
 	}
 }
